117be83add591e1f794a2e653c4a05e9789b13e9
xubeiyan <yueyongdong@gmail.com>
2023-07-19 10:19:39 +0800

    feature(src\layout\Navigation.vue): navigation click now can navigate to page

--- src/layout/Navigation.vue
+++ src/layout/Navigation.vue
@@ -1,15 +1,23 @@
 <script setup>
 const props = defineProps(['trail']);
 
+import { useRouter } from 'vue-router';
+const router = useRouter();
+
+const toLink = (path) => {
+  router.push(path);
+}
+
 const items = [
   { text: '主页', icon: 'mdi-home', link: '/' },
   { text: '关于', icon: 'mdi-account-multiple', link: '/about' },
 ];
+
 </script>
 <template>
   <v-navigation-drawer permanent :rail="$props.trail">
     <v-list nav>
-      <v-list-item v-for="(item, i) in items" :key="i" :value="item">
+      <v-list-item v-for="(item, i) in items" :key="i" :value="item" @click="toLink(item.link)">
         <template v-slot:prepend>
           <v-icon :icon="item.icon"></v-icon>
         </template>
