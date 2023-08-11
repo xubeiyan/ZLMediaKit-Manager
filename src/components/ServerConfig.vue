3ccd03d80ee2183f619044a50ba58ccd8953298a
xubeiyan <yueyongdong@gmail.com>
2023-08-11 09:58:58 +0800

    feature(ServerConfig.vue): add components/ServerConfig.vue and components/ServerStatistics.vue

---
+++ src/components/ServerConfig.vue
@@ -0,0 +1,61 @@
+<script setup>
+import { useMutation } from '@tanstack/vue-query';
+import { onMounted, reactive, computed } from 'vue';
+
+import api from '../utils/api';
+
+// 从pinia获取保存的后端地址和密钥
+import { useGlobalStore } from '../stores/global.js';
+const store = useGlobalStore();
+
+const config = reactive({
+  serverConifg: null,
+});
+
+const { isSuccess, error, mutate } = useMutation({
+  mutationFn: async () => {
+    const backEndpoint = computed(() => store.storage.backEndpoint).value;
+    const apiPrefix = computed(() => store.storage.apiPrefix).value;
+    const secret = computed(() => store.storage.secret).value;
+
+    let res = await api.get(`${backEndpoint}${apiPrefix}/api/getServerConfig`, {
+      params: {
+        secret,
+      }
+    });
+
+    if (res.code == -300) {
+      throw new Error('没有提供密钥');
+    } else if (res.code == -100) {
+      throw new Error('密钥错误');
+    }
+
+    config.serverConifg = res.data[0];
+  }
+});
+
+onMounted(() => {
+  mutate();
+});
+
+</script>
+
+<template>
+  <v-btn @click="mutate()">刷新</v-btn>
+  <v-divider class="my-2" />
+  <v-alert v-if="error" type="error">{{ error }}</v-alert>
+  <v-table density="compact" v-if="isSuccess">
+    <thead>
+      <tr>
+        <th>参数名</th>
+        <th>参数值</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr v-for="[key, value] in Object.entries(config.serverConifg)">
+        <td>{{ key }}</td>
+        <td>{{ value }}</td>
+      </tr>
+    </tbody>
+  </v-table>
+</template>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,12 @@
 <template>
-  <v-sheet>
-    
+  <v-sheet class="px-16 pt-8">
+    <v-card class="">
+      <v-card-title>关于此工具</v-card-title>
+      <v-card-subtitle>基于 MIT 协议开源的 ZLMediaKit 前端框架</v-card-subtitle>
+      <v-card-text>当前版本：ver 0.1.0</v-card-text>
+      <v-card-actions>
+        <v-btn icon="mdi-github"></v-btn>
+      </v-card-actions>
+    </v-card>
   </v-sheet>
 </template>
