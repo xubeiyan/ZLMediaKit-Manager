f76e431f705f7ca825661241b8a5c4f6432ca239
xubeiyan <yueyongdong@gmail.com>
2023-08-11 10:41:40 +0800

    fix(src/views/About.vue): add URL link to github icon and ZLMediaKit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,11 +1,18 @@
+<script setup>
+
+</script>
+
 <template>
   <v-sheet class="px-16 pt-8">
     <v-card class="">
       <v-card-title>关于此工具</v-card-title>
-      <v-card-subtitle>基于 MIT 协议开源的 ZLMediaKit 前端框架</v-card-subtitle>
+      <v-card-subtitle>基于 MIT 协议开源的 <a href="https://github.com/ZLMediaKit/ZLMediaKit" target="_blank">ZLMediaKit</a>
+        前端框架</v-card-subtitle>
       <v-card-text>当前版本：ver 0.1.0</v-card-text>
       <v-card-actions>
-        <v-btn icon="mdi-github"></v-btn>
+        <a href="https://github.com/xubeiyan/ZLMediaKit-Manager" target="_blank">
+          <v-btn icon="mdi-github" class="text-black"></v-btn>
+        </a>
       </v-card-actions>
     </v-card>
   </v-sheet>
