7e707c603f1a40a6f7c3350b2b61b0b08cebd95d
xubeiyan <yueyongdong@gmail.com>
2023-08-14 15:42:26 +0800

    feature(VideoDialog/VideoUrl.vue): add copy to clipboard button after every URL

--- src/components/VideoDialog/VideoUrl.vue
+++ src/components/VideoDialog/VideoUrl.vue
@@ -1,16 +1,56 @@
 <script setup>
+import { reactive } from 'vue';
+
 const prop = defineProps(['urls']);
+
+// 复制的地址
+const copied = reactive({
+  name: null,
+})
+
+// 点击复制按钮
+const handleIconClick = (name) => {
+  copied.name = name;
+  // navigation clipboard需要https环境，clipboard-write权限会自动授予活动标签页
+  document.execCommand('copy');
+}
+
 </script>
 <template>
   <v-text-field v-if="prop.urls.flv" label="http/flv 地址" hide-details="auto" density="compact"
-    :model-value="prop.urls.flv"></v-text-field>
+    :model-value="prop.urls.flv" @focus="$event.target.select()">
+    <template v-slot:append-inner>
+      <v-icon @click="handleIconClick('http/flv')" :icon="copied.name == 'http/flv' ? 'mdi-check' : 'mdi-content-copy'"
+        :color="copied.name == 'http/flv' ? 'green' : 'gray'" />
+    </template>
+  </v-text-field>
   <v-text-field v-if="prop.urls.mp4" label="http/mp4 地址" hide-details="auto" density="compact"
-    :model-value="prop.urls.mp4"></v-text-field>
-  <v-text-field v-if="prop.urls.ts" label="http/ts 地址" hide-details="auto" density="compact"
-    :model-value="prop.urls.ts"></v-text-field>
-  <v-text-field v-if="prop.urls.rtsp" label="rtsp 地址" hide-details="auto" density="compact"
-    :model-value="prop.urls.rtsp"></v-text-field>
-  <v-text-field v-if="prop.urls.rtmp" label="rtmp 地址" hide-details="auto" density="compact"
-    :model-value="prop.urls.rtmp"></v-text-field>
+    :model-value="prop.urls.mp4" @focus="$event.target.select()">
+    <template v-slot:append-inner>
+      <v-icon @click="handleIconClick('http/mp4')" :icon="copied.name == 'http/mp4' ? 'mdi-check' : 'mdi-content-copy'"
+        :color="copied.name == 'http/mp4' ? 'green' : 'gray'" />
+    </template>
+  </v-text-field>
+  <v-text-field v-if="prop.urls.ts" label="http/ts 地址" hide-details="auto" density="compact" :model-value="prop.urls.ts"
+    @focus="$event.target.select()">
+    <template v-slot:append-inner>
+      <v-icon @click="handleIconClick('http/ts')" :icon="copied.name == 'http/ts' ? 'mdi-check' : 'mdi-content-copy'"
+        :color="copied.name == 'http/ts' ? 'green' : 'gray'" />
+    </template>
+  </v-text-field>
+  <v-text-field v-if="prop.urls.rtsp" label="rtsp 地址" hide-details="auto" density="compact" :model-value="prop.urls.rtsp"
+    @focus="$event.target.select()">
+    <template v-slot:append-inner>
+      <v-icon @click="handleIconClick('rtsp')" :icon="copied.name == 'rtsp' ? 'mdi-check' : 'mdi-content-copy'"
+        :color="copied.name == 'rtsp' ? 'green' : 'gray'" />
+    </template>
+  </v-text-field>
+  <v-text-field v-if="prop.urls.rtmp" label="rtmp 地址" hide-details="auto" density="compact" :model-value="prop.urls.rtmp"
+    @focus="$event.target.select()">
+    <template v-slot:append-inner>
+      <v-icon @click="handleIconClick('rtmp')" :icon="copied.name == 'rtmp' ? 'mdi-check' : 'mdi-content-copy'"
+        :color="copied.name == 'rtmp' ? 'green' : 'gray'" />
+    </template>
+  </v-text-field>
 </template>
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -8,7 +8,7 @@
       <v-card-title>关于此工具</v-card-title>
       <v-card-subtitle>基于 MIT 协议开源的 <a href="https://github.com/ZLMediaKit/ZLMediaKit" target="_blank">ZLMediaKit</a>
         前端框架</v-card-subtitle>
-      <v-card-text>当前版本：ver 0.1.0</v-card-text>
+      <v-card-text>当前版本：ver 0.1.2</v-card-text>
       <v-card-actions>
         <a href="https://github.com/xubeiyan/ZLMediaKit-Manager" target="_blank">
           <v-btn icon="mdi-github" class="text-black"></v-btn>
